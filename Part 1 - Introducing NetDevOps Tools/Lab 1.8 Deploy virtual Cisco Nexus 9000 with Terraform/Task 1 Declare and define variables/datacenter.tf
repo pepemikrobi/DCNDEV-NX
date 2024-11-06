bc191760f8b1dbae7cb3f1866dfd7d769ca265b9
pepemikrobi <robert.slaski@gmail.com>
2024-11-06 14:15:08 +0100

    Lab 3.3 T2 typo fix

--- Part 1 - Introducing NetDevOps Tools/Lab 1.8 Deploy virtual Cisco Nexus 9000 with Terraform/Task 1 Declare and define variables/datacenter.tf
+++ Part 1 - Introducing NetDevOps Tools/Lab 1.8 Deploy virtual Cisco Nexus 9000 with Terraform/Task 1 Declare and define variables/datacenter.tf
@@ -6,3 +6,13 @@ data "vsphere_host" "esxi_host" {
     name          = var.esxi_hostname
     datacenter_id = data.vsphere_datacenter.datacenter.id
 }
+
+data "vsphere_resource_pool" "pool" {
+    name          = format("pod%s_dcndev", var.pod)
+    datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_datastore" "datastore" {
+    name          = var.esxi_datastore
+    datacenter_id = data.vsphere_datacenter.datacenter.id
+}
